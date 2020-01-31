2ac6c36396968b04b5cace69ab762eda1bed3edc
Diana Rukavishnikova <psihomatika@gmail.com>
2020-01-31 14:48:29 +0300

    fixed bugs after pull requests

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,41 @@ const path = require('path');
 const fs = require('fs');
 
 const dataPath = path.join(__dirname, '..', 'data', 'users.json');
+const usersData = fs.promises.readFile(dataPath, { encoding: 'utf8' });
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  usersData
+    .then((data) => {
+      const users = JSON.parse(data);
+      const user = users.find((item) => {
+        const { _id: itemId } = item;
+        return itemId === id;
+      });
+      if (user) {
+        res.send(user);
+      } else {
+        res.status(404).send({ message: 'Нет пользователя с таким id' });
+      }
+    })
+    .catch(() => res.status(500).send({ message: 'Не удалось получить данные пользователей' }));
+});
+
+router.get('/', (req, res) => {
+  usersData
+    .then((data) => res.send(JSON.parse(data)))
+    .catch(() => res.status(500).send({ message: 'Не удалось получить данные пользователей' }));
+});
+
+module.exports = router;
+
+/*
+const router = require('express').Router();
+const path = require('path');
+const fs = require('fs');
+
+const dataPath = path.join(__dirname, '..', 'data', 'users.json');
+
 const fsPromises = fs.promises;
 const usersData = fsPromises.readFile(dataPath, { encoding: 'utf8' });
 
@@ -31,3 +66,4 @@ router.get('/users', (req, res) => {
 });
 
 module.exports = router;
+*/
