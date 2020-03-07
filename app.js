df159cb7975093474e7cee64cb86daed91ad583d
Rukavishnikova Diana <r.diana74@gmail.com>
2020-03-07 22:38:17 +0300

    project work 15

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+require('dotenv').config(); // +
+const express = require('express'); // +
+const mongoose = require('mongoose'); // +
+const bodyParser = require('body-parser'); // +
+const cookieParser = require('cookie-parser'); // +
+const helmet = require('helmet');
+const { errors } = require('celebrate');
+
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
@@ -15,11 +19,33 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 const { PORT = 3000 } = process.env;
 
+app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(requestLogger);
+
 app.use('/', require('./routes/index'));
+
+app.use(errorLogger);
+
+app.use(errors());
+
+app.use((err, req, res, next) => {
+  if (!err.statusCode) {
+    const { statusCode = 500, message } = err;
+    res
+      .status(statusCode)
+      .send({
+        message: statusCode === 500
+          ? 'На сервере произошла ошибка'
+          : message,
+      });
+  }
+  res.status(err.statusCode).send({ message: err.message });
+  next();
+});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,42 @@
 const express = require('express');
+const { celebrate, Joi } = require('celebrate');
 
 const cardsRoutes = require('./cards');
 const usersRoutes = require('./users');
 const auth = require('../middlewares/auth');
+const NotFoundError = require('../errors/notFoundError');
 
 const router = express.Router();
 const { createUser, login } = require('../controllers/users');
 
-router.post('/signin', login);
-router.post('/signup', createUser);
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8).alphanum(),
+  }),
+}), login);
+
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8).alphanum(),
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().required(),
+  }),
+}), createUser);
 
 router.use('/cards', auth, cardsRoutes);
 router.use('/users', auth, usersRoutes);
 
-router.use('*', (req, res) => {
-  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+router.use('*', (req, res, next) => {
+  next(new NotFoundError('Запрашиваемый ресурс не найден'));
 });
 
 module.exports = router;
